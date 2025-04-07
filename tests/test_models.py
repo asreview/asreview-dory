f67cdee4ee3aba785430b3056369ea4ae7a6bb10
timovdk <5330531+timovdk@users.noreply.github.com>
2025-04-07 11:39:56 +0200

    Reduce test time by reusing feature matrices

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,3 @@
-from itertools import product
 from pathlib import Path
 
 import asreview as asr
@@ -22,45 +21,50 @@ feature_extractors = [
     fe for fe in extensions("models.feature_extractors") if "asreviewcontrib" in str(fe)
 ]
 
-# Generate all combinations of classifier and feature extractor
-pairs = list(product(classifiers, feature_extractors))
-
 test_ids = [
-    f"{classifier.name}__{feature_extractor.name}"
-    for classifier, feature_extractor in pairs
+    f"{feature_extractor.name}__per_classifier"
+    for feature_extractor in feature_extractors
 ]
 
 
-@pytest.mark.parametrize("classifier, feature_extractor", pairs, ids=test_ids)
-def test_asreview_simulation(classifier, feature_extractor):
+@pytest.mark.parametrize("feature_extractor", feature_extractors, ids=test_ids)
+def test_all_fe_clf_combinations(feature_extractor):
     # Load dataset
     data = asr.load_dataset(dataset_path)
 
-    # Define Active Learning Cycle
-    alc = asr.ActiveLearningCycle(
-        classifier=classifier.load()(),
-        feature_extractor=feature_extractor.load()(),
-        balancer=None,
-        querier=Max(),
-    )
+    # Preprocess data using feature extractor
+    fm = feature_extractor.load()().fit_transform(data)
 
-    # Run simulation
-    simulate = asr.Simulate(
-        X=data[["abstract", "title"]], labels=data["included"], cycles=[alc]
-    )
-    simulate.label([0, 1])
-    simulate.review()
+    # Test each classifier on the preprocessed FMs
+    for classifier in classifiers:
+        # Define Active Learning Cycle
+        alc = asr.ActiveLearningCycle(
+            classifier=classifier.load()(),
+            feature_extractor=feature_extractor.load()(),
+            balancer=None,
+            querier=Max(),
+        )
 
-    assert isinstance(simulate._results, pd.DataFrame)
-    assert simulate._results.shape[0] > 2 and simulate._results.shape[0] <= 6, (
-        "Simulation produced incorrect number of results."
-    )
-    assert classifier.name in simulate._results["classifier"].unique(), (
-        "Classifier is not in results."
-    )
-    assert feature_extractor.name in simulate._results["feature_extractor"].unique(), (
-        "Feature extractor is not in results."
-    )
+        # Run simulation
+        simulate = asr.Simulate(
+            X=fm,
+            labels=data["included"],
+            cycles=[alc],
+            skip_transform=True,
+        )
+        simulate.label([0, 1])
+        simulate.review()
+
+        assert isinstance(simulate._results, pd.DataFrame)
+        assert simulate._results.shape[0] > 2 and simulate._results.shape[0] <= 6, (
+            "Simulation produced incorrect number of results."
+        )
+        assert classifier.name in simulate._results["classifier"].unique(), (
+            "Classifier is not in results."
+        )
+        assert (
+            feature_extractor.name in simulate._results["feature_extractor"].unique()
+        ), "Feature extractor is not in results."
 
 
 def test_get_all_models():
